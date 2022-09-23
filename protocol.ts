9ae837e9986784111fa13de5b41dc23f4a083df0
Harsh Pandey <hpandey793@gmail.com>
2022-09-23 00:45:12 -0400

    maintain subscribers internally

--- protocol.ts
+++ protocol.ts
@@ -47,7 +47,6 @@ export interface Store {
   newState(roomId: RoomId, userId: UserId, data: ArrayBufferView): void;
   subscribeUser(roomId: RoomId, userId: UserId): void;
   unsubscribeUser(roomId: RoomId, userId: UserId): void;
-  unsubscribeAll(): void;
   onMessage(roomId: RoomId, userId: UserId, data: ArrayBufferView): void;
 }
 
@@ -63,6 +62,7 @@ export function register(config: RegisterConfig): Promise<CoordinatorClient> {
   const coordinatorHost = config.coordinatorHost ?? "coordinator.hathora.dev";
   const storeId = config.storeId ?? uuidv4();
   const { appSecret, authInfo, store } = config;
+  const subscribers: Map<RoomId, Set<UserId>> = new Map();
   return new Promise((resolve, reject) => {
     const socket = new net.Socket();
     let pingTimer: NodeJS.Timer;
@@ -70,7 +70,7 @@ export function register(config: RegisterConfig): Promise<CoordinatorClient> {
     socket.on("connect", () => {
       socket.write(JSON.stringify({ appSecret, storeId, authInfo }));
       const appId = createHash("sha256").update(appSecret).digest("hex");
-      const coordinatorClient = new CoordinatorClient(socket, coordinatorHost, appId, storeId);
+      const coordinatorClient = new CoordinatorClient(socket, coordinatorHost, appId, storeId, subscribers);
       if (pingTimer !== undefined) {
         console.log(`Reconnected to coordinator`);
       }
@@ -86,7 +86,11 @@ export function register(config: RegisterConfig): Promise<CoordinatorClient> {
     });
     socket.on("close", () => {
       console.error("Coordinator connection closed, retrying...");
-      store.unsubscribeAll();
+      subscribers.forEach((userIds, roomId) => {
+        userIds.forEach((userId) => {
+          store.unsubscribeUser(roomId, userId);
+        });
+      });
       if (pingTimer !== undefined) {
         clearInterval(pingTimer);
       }
@@ -96,13 +100,37 @@ export function register(config: RegisterConfig): Promise<CoordinatorClient> {
       const reader = new Reader(data);
       const type = reader.readUInt8();
       if (type === NEW_STATE) {
-        store.newState(reader.readUInt64(), reader.readString(), reader.readBuffer(reader.remaining()));
+        const [roomId, userId, data] = [
+          reader.readUInt64(),
+          reader.readString(),
+          reader.readBuffer(reader.remaining()),
+        ];
+        subscribers.set(roomId, new Set());
+        store.newState(roomId, userId, data);
       } else if (type === SUBSCRIBE_USER) {
-        store.subscribeUser(reader.readUInt64(), reader.readString());
+        const [roomId, userId] = [reader.readUInt64(), reader.readString()];
+        if (!subscribers.has(roomId)) {
+          subscribers.set(roomId, new Set());
+        }
+        subscribers.get(roomId)!.add(userId);
+        store.subscribeUser(roomId, userId);
       } else if (type === UNSUBSCRIBE_USER) {
-        store.unsubscribeUser(reader.readUInt64(), reader.readString());
+        const [roomId, userId] = [reader.readUInt64(), reader.readString()];
+        const subs = subscribers.get(roomId);
+        if (subs !== undefined) {
+          subs.delete(userId);
+          if (subs.size === 0) {
+            subscribers.delete(roomId);
+          }
+        }
+        store.unsubscribeUser(roomId, userId);
       } else if (type === MESSAGE) {
-        store.onMessage(reader.readUInt64(), reader.readString(), reader.readBuffer(reader.remaining()));
+        const [roomId, userId, data] = [
+          reader.readUInt64(),
+          reader.readString(),
+          reader.readBuffer(reader.remaining()),
+        ];
+        store.onMessage(roomId, userId, data);
       } else {
         throw new Error("Unknown type: " + type);
       }
@@ -111,7 +139,13 @@ export function register(config: RegisterConfig): Promise<CoordinatorClient> {
 }
 
 export class CoordinatorClient {
-  constructor(private socket: net.Socket, public host: string, public appId: AppId, public storeId: StoreId) {}
+  constructor(
+    private socket: net.Socket,
+    public host: string,
+    public appId: AppId,
+    public storeId: StoreId,
+    private subscribers: Map<RoomId, Set<UserId>>
+  ) {}
 
   public sendMessage(roomId: RoomId, userId: UserId, data: Buffer) {
     const userIdBuf = new Writer().writeString(userId).toBuffer();
@@ -136,6 +170,14 @@ export class CoordinatorClient {
         .writeBuffer(userIdBuf)
         .toBuffer()
     );
+  }
+
+  public getSubscribers(roomId: RoomId) {
+    const subs = this.subscribers.get(roomId);
+    if (subs === undefined) {
+      return [];
+    }
+    return [...subs];
   }
 
   _ping() {
